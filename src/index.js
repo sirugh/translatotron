27406dae1b8c6a33aadbbe0506ce38ab1772c66d
sirugh <rugh@adobe.com>
2020-09-23 16:29:04 -0500

    swap to sync csv reader
    Signed-off-by: sirugh <rugh@adobe.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const parse = require("csv-parse/lib/sync");
 
-const csv = require("csv-parser");
 const fs = require("fs");
 const path = require("path");
 
@@ -21,94 +20,85 @@ console.log("------------------------------\n");
 
 console.log(`Reading ${legacyFilePath}.`);
 
-const legacyKeyToNewKeyMap = {};
 const legacyTranslations = {};
-// TODO: come back and use csv-parse
-// const newTranslations = parse(fs.readFileSync(legacyFilePath, "utf8"), {
-// });
+parse(fs.readFileSync(legacyFilePath, "utf8"), {
+  columns: ["key", "value"],
+}).forEach(({ key, value }) => (legacyTranslations[key] = value));
 
-fs.createReadStream(legacyFilePath)
-  .pipe(csv(["key", "translation"]))
-  .on("data", ({ key, translation }) => {
-    // Spread each row into results. May overwrite if duplicates.
-    legacyTranslations[key] = translation;
-  })
-  .on("end", () => {
-    console.log("\n---- LEGACY TRANSLATIONS ----");
-    console.log(`${JSON.stringify(legacyTranslations, null, 2)}`);
-    console.log("------------------------------\n");
+console.log("\n---- LEGACY TRANSLATIONS ----");
+console.log(`${JSON.stringify(legacyTranslations, null, 2)}`);
+console.log("------------------------------\n");
 
-    const legacyStringtoKey = {};
-    Object.keys(legacyTranslations).forEach((key) => {
-      legacyStringtoKey[legacyTranslations[key]] = key;
+const legacyStringtoKey = {};
+Object.keys(legacyTranslations).forEach((key) => {
+  legacyStringtoKey[legacyTranslations[key]] = key;
+});
+
+const newStringToKey = {};
+Object.keys(newTranslations).forEach((key) => {
+  newStringToKey[newTranslations[key]] = key;
+});
+
+const legacyKeyToNewKeyMap = {};
+Object.keys(newStringToKey).forEach((key) => {
+  console.log(`Checking for legacy key for "${key}"...`);
+  const value = legacyStringtoKey[key];
+  if (value) {
+    console.log(
+      `  Found legacy key "${value}" with matching value as new key "${newStringToKey[key]}"`
+    );
+    legacyKeyToNewKeyMap[value] = newStringToKey[key];
+  } else {
+    console.log("  No results.");
+  }
+});
+console.log("\n---- LEGACY KEY:NEW KEY MAP ----");
+console.log(JSON.stringify(legacyKeyToNewKeyMap, null, 2));
+console.log("------------------------------\n");
+
+// Now that we have a map of keys we can use it to generate new
+// i18n assets for each existing csv.
+fs.readdir(legacyFolder, (err, files) => {
+  files.forEach((file) => {
+    const generatedFilePath = path.resolve(
+      generatedFolder,
+      `${file.split(".csv")[0]}.json`
+    );
+    console.log(`Generating new i18n for ${file} at ${generatedFilePath}`);
+    const legacyFile = fs.readFileSync(
+      path.resolve(legacyFolder, file),
+      "utf8"
+    );
+    const legacyCsvParsed = parse(legacyFile, {
+      columns: ["key", "value"],
     });
 
-    const newStringToKey = {};
-    Object.keys(newTranslations).forEach((key) => {
-      newStringToKey[newTranslations[key]] = key;
-    });
+    const generated = {};
 
-    Object.keys(newStringToKey).forEach((key) => {
-      console.log(`Checking for legacy key for "${key}"...`);
-      const value = legacyStringtoKey[key];
-      if (value) {
-        console.log(
-          `  Found legacy key "${value}" with matching value as new key "${newStringToKey[key]}"`
-        );
-        legacyKeyToNewKeyMap[value] = newStringToKey[key];
+    // Iterate over the legacy csv and add each to the new i18n object using
+    // the new key if one was found.
+    legacyCsvParsed.forEach((row) => {
+      const newKey = legacyKeyToNewKeyMap[row.key];
+      if (newKey) {
+        generated[legacyKeyToNewKeyMap[row.key]] = row.value;
       } else {
-        console.log("  No results.");
+        generated[row.key] = row.value;
       }
     });
-    console.log("\n---- LEGACY KEY:NEW KEY MAP ----");
-    console.log(JSON.stringify(legacyKeyToNewKeyMap, null, 2));
-    console.log("------------------------------\n");
 
-    // Now that we have a map of keys we can use it to generate new
-    // i18n assets for each existing csv.
-    fs.readdir(legacyFolder, (err, files) => {
-      files.forEach((file) => {
-        const generatedFilePath = path.resolve(
-          generatedFolder,
-          `${file.split(".csv")[0]}.json`
-        );
-        console.log(`Generating new i18n for ${file} at ${generatedFilePath}`);
-        const legacyFile = fs.readFileSync(
-          path.resolve(legacyFolder, file),
-          "utf8"
-        );
-        const legacyCsvParsed = parse(legacyFile, {
-          columns: ["key", "value"],
-        });
-
-        const generated = {};
-
-        // Iterate over the legacy csv and add each to the new i18n object using
-        // the new key if one was found.
-        legacyCsvParsed.forEach((row) => {
-          const newKey = legacyKeyToNewKeyMap[row.key];
-          if (newKey) {
-            generated[legacyKeyToNewKeyMap[row.key]] = row.value;
-          } else {
-            generated[row.key] = row.value;
-          }
-        });
-
-        const generatedKeys = Object.keys(generated);
-        // Iterate over new translations and add any that didn't match a legacy.
-        Object.keys(newTranslations).forEach((key) => {
-          if (!generatedKeys.includes(key))
-            generated[key] = newTranslations[key];
-        });
-
-        fs.writeFileSync(
-          generatedFilePath,
-          JSON.stringify(generated, null, 2),
-          (err) => {
-            if (err) throw err;
-          }
-        );
-        console.log(`  Wrote ${generatedFilePath}.`);
-      });
+    const generatedKeys = Object.keys(generated);
+    // Iterate over new translations and add any that didn't match a legacy.
+    Object.keys(newTranslations).forEach((key) => {
+      if (!generatedKeys.includes(key)) generated[key] = newTranslations[key];
     });
+
+    fs.writeFileSync(
+      generatedFilePath,
+      JSON.stringify(generated, null, 2),
+      (err) => {
+        if (err) throw err;
+      }
+    );
+    console.log(`  Wrote ${generatedFilePath}.`);
   });
+});
